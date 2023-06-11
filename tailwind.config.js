7cd5b82cc4d85933c2804b0223e7889758f9b168
Karsten Cole <kcole1371@bths.edu>
2023-06-11 19:54:28 -0400

    added mobile view for intro, changed fonts, changed pfp

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/index.html"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
       backgroundImage: {
         'galaxy-shooter': "url('/src/images/APCS_Final_Map.png')"
       }
