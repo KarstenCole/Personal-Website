e5231b57d3caa15aee47fccb2b77810795edb6b9
Karsten <kcole1371@bths.edu>
2023-07-11 23:35:06 -0400

    added the new Journey section

--- src/script.js
+++ src/script.js
@@ -6,3 +6,19 @@ function toggleMenu() {
     menu.classList.toggle('show');
     body.classList.toggle('show-scrollbar');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var wrapper = document.querySelector('.wrapper');
+    wrapper.addEventListener('wheel', function(event) {
+      var scrollTop = wrapper.scrollTop;
+      var scrollHeight = wrapper.scrollHeight;
+      var height = wrapper.clientHeight;
+  
+      var delta = (event.deltaY > 0) ? 1 : -1;
+  
+      if ((delta < 0 && scrollTop <= 0) || (delta > 0 && scrollTop + height >= scrollHeight)) {
+        event.preventDefault();
+      }
+    });
+});
+  
