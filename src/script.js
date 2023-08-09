d5c6d57b5e1958ae33ebab7ec7867c01d82f867a
Karsten <kcole1371@bths.edu>
2023-08-08 23:47:31 -0400

    changed menu color based on background

--- src/script.js
+++ src/script.js
@@ -13,9 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var scrollTop = wrapper.scrollTop;
       var scrollHeight = wrapper.scrollHeight;
       var height = wrapper.clientHeight;
-  
       var delta = (event.deltaY > 0) ? 1 : -1;
-  
       if ((delta < 0 && scrollTop <= 0) || (delta > 0 && scrollTop + height >= scrollHeight)) {
         event.preventDefault();
       }
